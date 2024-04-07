cd4f08f94b55db31ecf4ae29c4f998b8f0ba1878
Nelsonmsk <nelsonmsk@gmail.com>
2024-04-07 11:57:01 -0400

    Fix budget allocation app part 2

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,7 +3,7 @@ import { TiDelete } from 'react-icons/ti';
 import { AppContext } from '../context/AppContext';
 
 const ExpenseItem = (props) => {
-    const { dispatch } = useContext(AppContext);
+    const {currency, dispatch } = useContext(AppContext);
 
     const handleDeleteExpense = () => {
         dispatch({
@@ -48,7 +48,7 @@ const ExpenseItem = (props) => {
     return (
         <tr>
         <td>{props.name}</td>
-        <td>£{props.cost}</td>
+        <td>{currency}{props.cost}</td>
         <td><button onClick={event=> increaseAllocation(props.name)} style={{ addbtn}} >+</button></td>
         <td><button onClick={event=> decreaseAllocation(props.name)} style={{ subbtn}}>-</button></td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
